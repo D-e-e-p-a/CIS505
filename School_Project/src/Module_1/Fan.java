172e7a7fb3cb391bbf2d994b9dbfc54f5da6537c
Deepa <dnatarajan@my365.bellevue.edu>
2021-09-03 15:10:44 -0500

    "Assignment 1, Fan class"

--- School_Project/src/Module_1/Fan.java
+++ School_Project/src/Module_1/Fan.java
@@ -1,5 +1,66 @@
+
 package Module_1;
 
 public class Fan {
+	final static int SLOW = 1;
+	final static int MEDIUM = 2;
+	final static int FAST = 3;
+	private int speed ;
+	private boolean on ;
+	private double radius;
+	private String color ;
+
+	Fan() {
+		speed = Fan.SLOW;
+		on = false;
+		radius = 5;
+		color = "Blue";
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public boolean isOn() {
+		return on;
+	}
+
+	public void setOn(boolean on) {
+		this.on = on;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String toString() {
+
+		String message = null;
+
+		if (isOn()) {
+			message = "Fan Speed :" + "Fan is Off!" + '\n' + "Fan Color: " + getColor() + '\n' + "Fan Radius:"
+					+ getRadius();
+		} else {
+			message ="Fan Speed :" + getSpeed() + '\n' + "Fan Color: " + getColor() + '\n' + "Fan Radius:"
+					+ getRadius();
+		}
+		return message;
+	}
 
 }
--- School_Project/src/Module_1/TestFan.java
+++ School_Project/src/Module_1/TestFan.java
@@ -3,7 +3,24 @@ package Module_1;
 public class TestFan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Fan fan1=new Fan();
+		fan1.setSpeed(Fan.FAST);
+		fan1.setRadius(10);
+		fan1.setColor("Yellow");
+		fan1.setOn(true);
+		
+		Fan fan2=new Fan();
+		fan2.setSpeed(Fan.MEDIUM);
+		fan2.setRadius(5);
+		fan2.setColor("Blue");
+		fan2.setOn(false);
+		
+	
+		System.out.println("Fan 1" + '\n' +fan1.toString());
+		System.out.println();
+		System.out.println("Fan 2" +'\n'+ fan2.toString());
+		
 
 	}
 
