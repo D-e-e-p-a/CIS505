e52881140d458800c32d95d1a47e0ec108977342
Deepa <dnatarajan@my365.bellevue.edu>
2021-09-03 15:25:08 -0500

    "Hello word Modification"

---
+++ School_Project/src/Module_1/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package Module_1;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		String firstNameString="Deepa";
+		String lastNameString="Natarajan.";
+		
+		System.out.println( "Hello world- I am " + firstNameString + lastNameString);
+		
+	}
+
+}
