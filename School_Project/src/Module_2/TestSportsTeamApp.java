c5a3b449c16f9e392baed5ab4ac6c85259e6a9d2
Deepa <dnatarajan@my365.bellevue.edu>
2021-09-12 11:41:32 -0500

    Modified code

--- School_Project/src/Module_2/TestSportsTeamApp.java
+++ School_Project/src/Module_2/TestSportsTeamApp.java
@@ -4,38 +4,62 @@ import java.util.Scanner;
 
 public class TestSportsTeamApp {
 	static Team team1;
+	static String choice = "y";
+	static Scanner sc = new Scanner(System.in); // scanner variable to get the system inputs.
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); //scanner variable to get the system inputs.
-		System.out.println("  "+"Welcome to the sports Team App" + '\n');
-		String choice = "y";
+
+		System.out.println("  " + "Welcome to the sports Team App" + '\n');
+
 		while (choice.equalsIgnoreCase("y")) {
-
-			System.out.println("  "+"Enter the Team Name: ");
-			team1 = new Team(sc.next());//Assigns the team name
-			{
-				System.out.println('\n' +"  "+ "Enter the Players Name: ");
-				System.out.println("  "+"Hint: use comma for multiple players; no space>:");
-				String input[] = sc.next().split(",");
-				{
-					for (int i = 0; i < input.length; i++)
-						team1.addPlayer(input[i]);//Adding the player names
-				}
-			}
-
-			System.out.println('\n' + "  "+"--Team Summary---");
-
-			System.out.println("  "+"Number of Players in the team:" + team1.getPlayerCount());
-			System.out.println("  "+"Players of the Team:" + team1.printPlayersNames());
-
-			System.out.print('\n' +"  "+ "Continue? (y/n): ");
-			choice = sc.next();
-			sc.close();//closing the scanner variable
-			System.out.println();
+			
+			takingValues();
+			teamSummary();
+			MoreTeam();
 
 		}
 
 		System.out.println("End of the line...");
+		sc.close();// closing the scanner variable
+
+	}
+
+	// utility methods
+
+	private static void MoreTeam() {
+		System.out.print('\n' + "  " + "Continue? (y/n): ");
+		choice = sc.next();
+		
+		System.out.println();
+
+	}
+
+	private static void takingValues() {
+
+		System.out.println("  " + "Enter the Team Name: ");
+		team1 = new Team(sc.next());// Assigns the team name
+		System.out.println('\n' + "  " + "Enter the Players Name: ");
+		System.out.println("  " + "Hint: use comma for multiple players; no space>:");
+
+		addPlayersToList(sc.next());
+
+	}
+
+	public static void addPlayersToList(String in) {
+
+		String input[] = in.split(",");
+		{
+			for (int i = 0; i < input.length; i++)
+				team1.addPlayer(input[i]);// Adding the player names
+		}
+
+	}
+
+	public static void teamSummary() {
+		System.out.println('\n' + "  " + "--Team Summary---");
+
+		System.out.println("  " + "Number of Players in the team:" + team1.getPlayerCount());
+		System.out.println("  " + "Players of the Team:" + team1.printPlayersNames());
 
 	}
 
